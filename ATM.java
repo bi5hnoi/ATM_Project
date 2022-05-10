6ed6991c90283530b0724d31f3cdfa0c51f08ed9
Rahul Bishnoi <76421412+bi5hnoi@users.noreply.github.com>
2022-05-10 09:52:18 +0530

    Add files via upload

---
+++ ATM.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+public class ATM {
+	public static void main(String[] args) throws IOException {
+		OptionMenu optionMenu = new OptionMenu();
+		System.out.println("Welcome to the ATM Project!");
+		optionMenu.mainMenu();
+	}
+}
